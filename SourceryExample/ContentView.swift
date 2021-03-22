d358220afd1b3ce79f36c115aa5b9dc171a58c44
Harris Borawski <harris_borawski@intuit.com>
2021-03-22 14:20:01 -0700

    Initial Commit

---
+++ SourceryExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceryExample
+//
+//  Created by Harris Borawski on 3/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceryExample/SourceryExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceryExampleApp.swift
+//  SourceryExample
+//
+//  Created by Harris Borawski on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceryExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
